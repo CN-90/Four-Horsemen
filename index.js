657c5f41addf0ce44f50af4491221165b1a54e50
Christian <chr.revel@gmail.com>
2019-12-30 08:20:41 -0700

    Added media queries for contact section, added menu modal for smaller screens

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// GOOGLE MAP
+
 function initMap() {
   // Styles a map in night mode.
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -91,3 +93,34 @@ function initMap() {
     title: 'Four Horsemen'
   });
 }
+
+// MENU MODAL
+let menuBurger = document.getElementById('hamburger');
+let menuModal = document.getElementById('menu-modal');
+let exitModal = document.getElementById('exit-modal');
+let modaloptions = document.getElementById('menu-modal-options');
+let modalOption = document.getElementsByClassName('menu-modal-option');
+
+menuBurger.onclick = () => {
+  menuModal.style.display = 'block';
+  document.body.style.overflowY = 'hidden';
+};
+
+exitModal.onclick = () => {
+  menuModal.style.display = 'none';
+  document.body.style.overflowY = 'scroll';
+};
+
+window.onclick = e => {
+  if (e.target === modaloptions) {
+    menuModal.style.display = 'none';
+    document.body.style.overflowY = 'scroll';
+  }
+};
+
+Array.from(modalOption).forEach(option => {
+  option.onclick = () => {
+    menuModal.style.display = 'none';
+    document.body.style.overflowY = 'scroll';
+  };
+});
